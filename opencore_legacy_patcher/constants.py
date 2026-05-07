c0d9fd88a27e58a7bf1f9d428c39a1ddf5e82238
Dani Arifin <danithesillyfem@gmail.com>
2026-05-07 20:00:40 +0700

    Update constants.py

--- opencore_legacy_patcher/constants.py
+++ opencore_legacy_patcher/constants.py
@@ -14,7 +14,7 @@ class Constants:
     def __init__(self) -> None:
         # Patcher Versioning
         self.patcher_version:                 str = "3.0.0"  # OpenCore-Custom-Patcher
-        self.patcher_support_pkg_version:     str = "1.9.6"  # PatcherSupportPkg
+        self.patcher_support_pkg_version:     str = "2.0.0"  # PatcherSupportPkg
         self.copyright_date:                  str = "Copyright © 2020-2026 Dortania + DaniTheSillyFem 2025-2026"
         self.patcher_name:                    str = "OpenCore Custom Patcher"
 
